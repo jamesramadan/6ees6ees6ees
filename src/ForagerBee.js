ac70572d61f7c92cd821a3d6db35ddf29a06d7f5
jamesramadan <jamesramadan@gmail.com>
2016-06-14 17:06:43 -0700

    Completed 6ees 6ees 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,6 +1,16 @@
 import Bee from './Bee'
 
-class ForagerBee {
+class ForagerBee extends Bee {
+  constructor(){
+    super();
+    this.age =10;
+    this.job = "find pollen";
+    this.canFly = true;
+    this.treasureChest = [];
+  };
+  forage(treasure){
+    this.treasureChest.push(treasure);
+  };
   // TODO..
 };
 
